ea138001057400a094ac9176366323215e925867
AAYUSH KUMAR <121863336+aayushdebugging@users.noreply.github.com>
2024-12-21 09:38:57 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+
+
+app = FastAPI()
+
+@app.get('/')
+async def read_root():
+    return {"message":"Hello World"}
+
+@app.get('/greet')
+async def greet_name(name:Optional[str] = "User" , age:int = 0 ) -> dict:
+    return {"message":f"Hello {name}" , "age":age}  ## fThe f in f"Hello {name}" is used for f-string formatting in Python
+
+
+class BookCreateModel(BaseModel):
+    title :str
+    author:str
+    
+@app.post('/create_book')
+async def create_book(book_data:BookCreateModel):
+    return{
+        "title":book_data.title,
+        "author":book_data.author
+    }
+
+@app.get('/get_headers')
+async def get_headers():
+    pass
